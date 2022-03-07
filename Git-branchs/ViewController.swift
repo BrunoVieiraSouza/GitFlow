c9426f0b4cd6a94ca5076dccdc721910caf1b791
Bruno Vieira Souza <bvsouza@yahoo.com.br>
2022-03-07 14:44:04 -0400

    Revert "teste para reverter antes de dar um Push"
    This reverts commit 755880eafeb5e34c80b053e577def964a4a3ed22.

--- Git-branchs/ViewController.swift
+++ Git-branchs/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 class ViewController: UIViewController {
     
     var main = "Main branch"
-    
-    var alteracoes = "teste"
 
     override func viewDidLoad() {
         super.viewDidLoad()
