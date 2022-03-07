332bc7c7535d362512a2538ffe0969f4c6ac4848
Bruno Vieira Souza <bvsouza@yahoo.com.br>
2022-03-07 13:54:49 -0400

    primeiro commit

---
+++ Git-branchs/Main/Main.swift
@@ -0,0 +1,12 @@
+//
+//  Main.swift
+//  Git-branchs
+//
+//  Created by Bruno Vieira Souza on 07/03/22.
+//
+
+import Foundation
+
+struct Main {
+    var main: String
+}
---
+++ Git-branchs/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Git-branchs
+//
+//  Created by Bruno Vieira Souza on 07/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var main = "Main branch"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
