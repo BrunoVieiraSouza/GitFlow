e8b97815ae75a80e1135aea85323634601044e37
Bruno Vieira Souza <bvsouza@yahoo.com.br>
2022-03-07 14:46:09 -0400

    apagando viewDidLoad

--- Git-branchs/ViewController.swift
+++ Git-branchs/ViewController.swift
@@ -10,12 +10,5 @@ import UIKit
 class ViewController: UIViewController {
     
     var main = "Main branch"
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
 
